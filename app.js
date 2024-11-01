4b0b61778c2b8b17d4d57faac0ee88166fc3df08
Camden <176319147+cmwestfall@users.noreply.github.com>
2024-11-01 14:47:05 -0400

    final commit

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const givenArray = [1, 2, 3, 4, 5];
+const givenArray = [7, -2, 3, 4, 5, 1, 23, 23];
 
-let maxValue = 0;
+// in case of all negatives
+let maxValue = -Infinity;
+let secondMaxValue = -Infinity;
+
+let answer = [];
 
 for (let i = 0; i < givenArray.length; i++) {
   if (givenArray[i] > maxValue) {
+    secondMaxValue = maxValue;
     maxValue = givenArray[i];
+  } else if (givenArray[i] <= maxValue && givenArray[i] > secondMaxValue) {
+    secondMaxValue = givenArray[i];
   }
 }
 
-console.log(maxValue);
+answer.push(maxValue, secondMaxValue);
+console.log(answer);
